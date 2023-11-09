33f29def0c092788795ad179d3be5a49d590b58b
darderius <33958872+darderius@users.noreply.github.com>
2023-11-09 21:07:41 +0100

    Update JenkinsUtils.groovy

--- groovy/JenkinsUtils.groovy
+++ groovy/JenkinsUtils.groovy
@@ -109,7 +109,7 @@ void createDeployment(customProperties) {
 
 void requestPatchAnalysis(customProperties) {
   aquamanUrl = SITEBASEURL + "/webapi/analysePatch?applicationUuid=b62d65c9-d656-42c3-b480-83ebb9f4fb4a" 
-   response=sh( script:"curl --location  --request POST \"$aquamanUrl\" --header \"Appian-Document-Name:finalPackage.zip\" --header \"Appian-API-Key: $APIKEY\" --form \"json={\"packageFileName\":\"finalPackage.zip\",\"name\":\"$DEPLOYMENTNAME\"}\"", returnStdout: true).trim()
+   response=sh( script:"curl --location  --request POST \"$aquamanUrl\" --header \"Appian-Document-Name:finalPackage.zip\" --header \"Appian-API-Key: $APIKEY\" --data-binary @\"adm/finalPackage.zip\" --form \"json={\"packageFileName\":\"finalPackage.zip\",\"name\":\"$DEPLOYMENTNAME\"}\"", returnStdout: true).trim()
    println "Respuesta recibida"
     println response
   //.readLines().drop(1).join(" ")
