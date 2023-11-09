b30a8ef64fa7d6bf5143bed07e77078ad4c020a0
darderius <33958872+darderius@users.noreply.github.com>
2023-11-09 23:16:09 +0100

    Update JenkinsUtils.groovy

--- groovy/JenkinsUtils.groovy
+++ groovy/JenkinsUtils.groovy
@@ -135,7 +135,6 @@ void checkAnalyzePatchStatus(patchId) {
   while (!statusVar.equals("Completed")) {
     sleep 30
     analysisStatus = sh(script: "curl --silent --location --request GET \"$newUrl\" --header \"Appian-API-Key: $APIKEY\"" , returnStdout: true).trim()
-    analysisStatus = analysisStatus.readLines().drop(1).join(" ")
     analysisStatusJson = new groovy.json.JsonSlurperClassic().parseText(analysisStatus)
     statusVar = analysisStatusJson.status
   }
